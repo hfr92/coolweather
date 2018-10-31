28defea826f4fdf3b3f98fe8a5ee5b0b3cef96b9
hfr <731035181@qq.com>
2018-10-31 10:50:57 +0800

    加入显示天气的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by hp on 2018/10/30.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity {
+        public String aqi;
+
+        public String pm25;
+    }
+
+}
